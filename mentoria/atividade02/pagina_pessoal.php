e35ee719654bc03b58e1fe8e1bddf1bbe6420258
Kérisson Falcão <kerisson_falcao@hotmail.com>
2017-04-04 11:55:24 -0300

    atividade2

---
+++ mentoria/atividade02/pagina_pessoal.php
@@ -0,0 +1,55 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PÁGINA PESSOAL</title>
+<link href="css/style.css" rel="stylesheet" type="text/css"/>
+
+</head>
+
+<body>
+<div id="conteudo">
+<div class="foto">
+<h1> &nbsp; &nbsp;Kérisson Falcão</h1>
+<img src="./img/1orelhinha.jpg" alt="Texto alternativo" title="Avatar" />
+</div>
+<div class="h2"> <h2> &nbsp; &nbsp; &nbsp;Quem Sou</h2> </div>
+<p> &nbsp; &nbsp; &nbsp;Sou um intusiatas pela ciência da computação, gosto de tecnologias de modo geral. Atualmente estou no 9° período do curso. Sou de Amarante, mas me mudei para Imperatriz pra
+poder fazer o curso. Tenho x anos rs. Trabalhei na Caema e exerci outros cargos.</p>
+
+
+<div class="h2"><h2> &nbsp; &nbsp; &nbsp;Interesses Profissionais</h2></div>
+<p> &nbsp; &nbsp; &nbsp; No momento estou trabalhando na monografia, ao concluir pretendo ingressar na carreira de desenvolvedor, provavelmente com trabalho remoto, freelancer, algo assim. Como obetivo final pretendo trabalhar na minha área de formação, na carreira pública. As motivações são bons salários e estabilidade, porém vou tentar continuar com o trabalho como freelancer.</p>
+
+
+
+<div class="h2"><h2> &nbsp; &nbsp; &nbsp;Interesses Pessoais</h2></div>
+<p> &nbsp; &nbsp; &nbsp; Como todo mundo, pretendo ganhar um bom salário futuramente, ter uma boa casa, um bom automóvel e construir um bela família.  Vou dedicar um tempo para alguns hobes, como tocar teclado , bateria etc. Por último e não menos importante, viajar bastante, conhecer outros países e outras cidades no Brasil.</p>
+
+<div class="h2"><h2 > &nbsp; &nbsp; &nbsp;Escolaridade</h2></div>
+<p>Graduando.</p>
+
+<div class="h2"> <h2> &nbsp; &nbsp; &nbsp;Onde Estuda(ou)</h2></div>
+<p>  IFMA - Instituto Federal do Maranhão.</p>
+
+<div class="h2"><h2> &nbsp; &nbsp; &nbsp;Cursando</h2></div>
+<p> Ciência da Computação. </p>
+
+<div class="h2"><h2> &nbsp; &nbsp; &nbsp;Linguagens de programação de interesse </h2></div>
+<ul>
+<li>C</li>
+<li>C++</li>
+<li>Java</li>
+<li>Python</li>
+<li>Php</li>
+</ul>
+
+
+
+
+<a href="https://www.facebook.com/kerisson.falcaodacunha" title="facebook">facebook</a>&nbsp; &nbsp;
+<a href="https://github.com/kerisson/" title="GitHub" target="_top">gitHub</a>
+
+</div>
+</body>
+</html>
